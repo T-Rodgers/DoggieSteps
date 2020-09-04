ee071cfe228cd1b7e25f475180fa64c138c4ed96
Terrence Rodgers <tdr0082@gmail.com>
2020-09-04 18:51:48 -0500

    Add necessary views to run on device
    Added ButterKnife view binding so we could test new layout
    on device.

--- app/src/main/java/com/tdr/app/doggiesteps/MainActivity.java
+++ app/src/main/java/com/tdr/app/doggiesteps/MainActivity.java
@@ -3,12 +3,26 @@ package com.tdr.app.doggiesteps;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_bar)
+    BottomAppBar bottomAppBar;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
