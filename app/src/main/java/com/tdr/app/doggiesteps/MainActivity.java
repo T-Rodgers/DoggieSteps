d62d5712d317ec37d383b2988502f3f96f371b03
Terrence Rodgers <tdr0082@gmail.com>
2020-09-10 15:14:29 -0500

    Implement onTabSelectedListener
    Added listener that is called when each tab is pressed.

--- app/src/main/java/com/tdr/app/doggiesteps/MainActivity.java
+++ app/src/main/java/com/tdr/app/doggiesteps/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tdr.app.doggiesteps;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,7 +12,7 @@ import com.google.android.material.tabs.TabLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @BindView(R.id.bottom_bar)
     BottomAppBar bottomAppBar;
@@ -27,6 +28,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        Toast.makeText(getBaseContext(), "Home has been selected",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case 1:
+                        Toast.makeText(getBaseContext(), "Favorites has been selected",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        Toast.makeText(getBaseContext(), "What do I do?",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 }
