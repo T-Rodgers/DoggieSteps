e62c8cc2263174cc0aa3451f17137afa736866be
Terrence Rodgers <tdr0082@gmail.com>
2020-09-22 16:50:18 -0500

    Implement emptyView for when list is empty

--- app/src/main/java/com/tdr/app/doggiesteps/fragments/PetListFragment.java
+++ app/src/main/java/com/tdr/app/doggiesteps/fragments/PetListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,6 +29,10 @@ public class PetListFragment extends Fragment {
 
     @BindView(R.id.pet_list_recycler_view)
     RecyclerView recyclerView;
+    @BindView(R.id.empty_view_photo)
+    ImageView emptyViewPhoto;
+    @BindView(R.id.empty_view_message)
+    TextView emptyViewText;
 
     private DogListAdapter adapter;
     private DogListViewModel dogListViewModel;
@@ -43,6 +49,8 @@ public class PetListFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+        int count = adapter.getItemCount();
+
 
 
 
@@ -50,9 +58,21 @@ public class PetListFragment extends Fragment {
         dogListViewModel.getAllDogs().observe(getViewLifecycleOwner(), new Observer<List<Dog>>() {
             @Override
             public void onChanged(List<Dog> dogs) {
+                if (dogs.size() == 0) {
+                    emptyViewPhoto.setVisibility(View.VISIBLE);
+                    emptyViewText.setVisibility(View.VISIBLE);
+                } else {
+                    emptyViewPhoto.setVisibility(View.GONE);
+                    emptyViewText.setVisibility(View.GONE);
+                }
                 adapter.setDogList(dogs);
             }
         });
+
+        if (count == 0) {
+            emptyViewPhoto.setVisibility(View.VISIBLE);
+            emptyViewText.setVisibility(View.VISIBLE);
+        }
 
         return rootView;
     }
