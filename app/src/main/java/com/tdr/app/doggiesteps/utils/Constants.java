aa3408cac57368606a42209ceebe64d838fa08a1
Terrence Rodgers <tdr0082@gmail.com>
2020-09-21 23:10:39 -0500

    Update Constants.java and strings.xml

--- app/src/main/java/com/tdr/app/doggiesteps/utils/Constants.java
+++ app/src/main/java/com/tdr/app/doggiesteps/utils/Constants.java
@@ -2,6 +2,10 @@ package com.tdr.app.doggiesteps.utils;
 
 public class Constants {
 
-    public static final String DOG_NAME = "name";
-    public static final String DOG_BREED = "breed";
+    public static final String EXTRA_DOG_NAME = "name";
+    public static final String EXTRA_DOG_BREED = "breed";
+    public static final String EXTRA_DOG_AGE = "age";
+    public static final String EXTRA_DOG_BIO = "bio";
+
+    public static final int NEW_PET_REQUEST_CODE = 1;
 }
