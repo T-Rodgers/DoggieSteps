1907086825e646ac5bdb3972e78fc9deb494495d
Terrence Rodgers <tdr0082@gmail.com>
2020-10-07 14:12:13 -0500

    Update strings, Add FileProvider in manifest.

--- app/src/main/java/com/tdr/app/doggiesteps/utils/Constants.java
+++ app/src/main/java/com/tdr/app/doggiesteps/utils/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String EXTRA_SAVED_PET = "saved_pet";
     public static final int NEW_PET_REQUEST_CODE = 1;
     public static final String EXTRA_SELECTED_PET = "selected_pet" ;
+    public static final int REQUEST_IMAGE_CAPTURE = 2;
 }
