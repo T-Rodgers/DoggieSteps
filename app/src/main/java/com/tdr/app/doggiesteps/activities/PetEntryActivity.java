0693fe8dfe0d34884fcb4bc14662ed7d2057f68f
Terrence Rodgers <tdr0082@gmail.com>
2020-10-07 14:11:23 -0500

    Add views for adding an image

--- app/src/main/java/com/tdr/app/doggiesteps/activities/PetEntryActivity.java
+++ app/src/main/java/com/tdr/app/doggiesteps/activities/PetEntryActivity.java
@@ -1,24 +1,50 @@
 package com.tdr.app.doggiesteps.activities;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 import com.tdr.app.doggiesteps.R;
 import com.tdr.app.doggiesteps.model.Dog;
 import com.tdr.app.doggiesteps.utils.Constants;
+import com.tdr.app.doggiesteps.utils.GlideUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PetEntryActivity extends AppCompatActivity {
 
+    private static final String TAG = PetEntryActivity.class.getSimpleName();
+
+    String currentPhotoPath;
+    private Uri mUri;
+
+    @BindView(R.id.empty_add_photo_background)
+    View emptyViewBackground;
+    @BindView(R.id.petImage)
+    ImageView petImage;
     @BindView(R.id.details_snackbar_view)
     View contextView;
     @BindView(R.id.pet_name_edittext)
@@ -31,6 +57,8 @@ public class PetEntryActivity extends AppCompatActivity {
     EditText bioEntry;
     @BindView(R.id.save_button)
     Button saveButton;
+    @BindView(R.id.add_photo_button)
+    Button addPhotoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +67,9 @@ public class PetEntryActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         saveButton.setOnClickListener(v -> savePet());
+        addPhotoButton.setOnClickListener(v -> {
+
+        });
     }
 
     public void savePet() {
