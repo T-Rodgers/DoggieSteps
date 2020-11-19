cd9b7b9681b6988159df1106c0bb111685aead5b
Terrence Rodgers <tdr0082@gmail.com>
2020-11-19 14:11:19 -0600

    Widget implementation (Continued)

--- app/src/main/java/com/tdr/app/doggiesteps/utils/DogAppWidget.java
+++ app/src/main/java/com/tdr/app/doggiesteps/utils/DogAppWidget.java
@@ -1,8 +1,22 @@
 package com.tdr.app.doggiesteps.utils;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.RemoteViews;
+
+import com.tdr.app.doggiesteps.R;
 
 public class DogAppWidget extends AppWidgetProvider {
 
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.dog_app_widget);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        // TODO: Finish creating widget, add method and icon to add widget using SharedPreferences
+
+    }
 }
