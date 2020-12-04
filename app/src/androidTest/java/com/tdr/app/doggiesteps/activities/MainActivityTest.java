d460cdabedad3d32dc1900c3204ea0e4d11fcc17
Terrence Rodgers <tdr0082@gmail.com>
2020-12-04 14:37:59 -0600

    Editing Tests for MainActivity and PetEntryActivity

--- app/src/androidTest/java/com/tdr/app/doggiesteps/activities/MainActivityTest.java
+++ app/src/androidTest/java/com/tdr/app/doggiesteps/activities/MainActivityTest.java
@@ -1,13 +1,9 @@
 package com.tdr.app.doggiesteps.activities;
 
-import androidx.fragment.app.testing.FragmentScenario;
-import androidx.lifecycle.Lifecycle;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.tdr.app.doggiesteps.R;
-import com.tdr.app.doggiesteps.fragments.PetListFragment;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,9 +11,9 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -25,25 +21,17 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
 
-
-    @Test
-    public void recyclerViewClick() {
-        FragmentScenario<PetListFragment> fragmentScenario =
-                FragmentScenario.launchInContainer(PetListFragment.class);
-        fragmentScenario.moveToState(Lifecycle.State.CREATED);
-
-        onView(withId(R.id.pet_list_recycler_view))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-
-        onView(withId(R.id.details_pet_name)).check(matches(withText("Scooby-Doo")));
-        onView(withId(R.id.details_pet_breed)).check(matches(withText("Great Dane")));
-    }
-
-
     @Test
     public void testAddPet() {
         onView(withId(R.id.fab))
                 .perform(click());
+    }
+
+    @Test
+    public void testViewPagerTab() {
+        onView(allOf(withId(R.id.tab_item_favorites), isDisplayed()))
+                .perform(click());
+
     }
 
 }
---
+++ app/src/androidTest/java/com/tdr/app/doggiesteps/activities/PetDetailsActivityTest.java
@@ -0,0 +1,18 @@
+package com.tdr.app.doggiesteps.activities;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class PetDetailsActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<PetDetailsActivity> scenarioRule =
+            new ActivityScenarioRule<>(PetDetailsActivity.class);
+
+    @Test
+    public void testWalkButton() {
+
+    }
+}
