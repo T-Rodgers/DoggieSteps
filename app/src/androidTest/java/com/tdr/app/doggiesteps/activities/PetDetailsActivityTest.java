192f786acf8644e71a116f1d758ffc4ae1df6bd0
Terrence Rodgers <tdr0082@gmail.com>
2020-12-14 20:55:05 -0600

    Update test for added buttons

--- app/src/androidTest/java/com/tdr/app/doggiesteps/activities/PetDetailsActivityTest.java
+++ app/src/androidTest/java/com/tdr/app/doggiesteps/activities/PetDetailsActivityTest.java
@@ -1,5 +1,6 @@
 package com.tdr.app.doggiesteps.activities;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -10,7 +11,13 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.tdr.app.doggiesteps.utils.Constants.TEST_PET_AGE;
+import static com.tdr.app.doggiesteps.utils.Constants.TEST_PET_BIO;
+import static com.tdr.app.doggiesteps.utils.Constants.TEST_PET_BREED;
+import static com.tdr.app.doggiesteps.utils.Constants.TEST_PET_NAME;
 
 public class PetDetailsActivityTest {
 
@@ -31,5 +38,20 @@ public class PetDetailsActivityTest {
                 .perform(click());
         onView(withId(R.id.details_favorite_button))
                 .perform(click());
+        onView(withId(R.id.add_widget_icon))
+                .perform(click());
+    }
+
+    @Test
+    public void openPetEntry_verifyText() {
+        onView(withId(R.id.pet_list_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.edit_pet_icon))
+                .perform(click());
+
+        onView(withId(R.id.pet_name_edittext)).check(matches(withText(TEST_PET_NAME)));
+        onView(withId(R.id.pet_breed_edittext)).check(matches(withText(TEST_PET_BREED)));
+        onView(withId(R.id.pet_age_edittext)).check(matches(withText(TEST_PET_AGE)));
+        onView(withId(R.id.pet_bio_edittext)).check(matches(withText(TEST_PET_BIO)));
     }
 }
